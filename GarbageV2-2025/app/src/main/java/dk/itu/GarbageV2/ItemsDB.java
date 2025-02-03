6006e5c4e07e4f01e08a2cd4164894bf2d011209
Dorten8 <jsej@itu.dk>
2025-02-03 14:07:04 +0100

    duplicated GarbageV1 project into GarbageV2

---
+++ GarbageV2-2025/app/src/main/java/dk/itu/GarbageV2/ItemsDB.java
@@ -0,0 +1,40 @@
+package dk.itu.GarbageV2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ItemsDB {
+    private List<Item> ItemsDB= new ArrayList<>();
+
+    public ItemsDB() { }
+
+    public String listItems() {
+        String r= "";
+        for(Item i: ItemsDB)
+            r= r+"\n Buy " + i.toString();
+        return r;
+    }
+
+public String findCategory(String item){
+
+        for (Item i:ItemsDB){
+            if (i.getWhat().equals(item))
+                return i.getWhere();
+        }
+        return "Not found";
+
+}
+
+    // Will be used later
+    public void addItem(String what, String where){
+        ItemsDB.add(new Item(what, where));
+    }
+
+    public void fillItemsDB() {
+        ItemsDB.add(new Item("Milk Carton", "Food"));
+        ItemsDB.add(new Item("Carrots", "Organic"));
+        ItemsDB.add(new Item("Can", "Metal"));
+        ItemsDB.add(new Item("Bottle", "Glass"));
+        ItemsDB.add(new Item("Plate", "Porcelain"));
+    }
+}

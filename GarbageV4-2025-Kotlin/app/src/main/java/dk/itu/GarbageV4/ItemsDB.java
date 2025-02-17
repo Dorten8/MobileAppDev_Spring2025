58bfc50876aa95a3f55022390f59413ebad730af
Dorten8 <jsej@itu.dk>
2025-02-17 17:08:21 +0100

    Reapply .gitignore

---
+++ GarbageV4-2025-Kotlin/app/src/main/java/dk/itu/GarbageV4/ItemsDB.java
@@ -0,0 +1,75 @@
+package dk.itu.GarbageV4;
+
+import android.content.Context;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+
+public class ItemsDB {
+    private static ItemsDB sItemsDB;
+
+    private final HashMap<String, String> ItemsDB_map = new HashMap<String, String>();
+
+    private static Context context;
+
+    private ItemsDB(){
+        if(context == null){
+            throw new IllegalStateException("context must be set first!");
+        }
+        fillItemsDB(context, "garbage.txt");
+    }
+
+    public static void setContext(Context aContext) {
+        context = aContext;
+    }
+
+    // Singleton pattern get implementation
+    public static ItemsDB get(){
+        if (sItemsDB == null) sItemsDB = new ItemsDB();
+        return sItemsDB;
+    }
+
+    public HashMap<String, String> getItemsDB_map() {
+        return ItemsDB_map;
+    }
+
+    public String listItems() {
+        String r= "";
+        for(HashMap.Entry<String, String> item : ItemsDB_map.entrySet())
+            r= r+"\n Put " + item.getKey() + " in: " + item.getValue();
+        return r;
+    }
+
+    public String findCategory(String item){
+
+        for (HashMap.Entry<String, String> i : ItemsDB_map.entrySet()){
+            if (i.getKey().equals(item))
+                return i.getValue();
+        }
+        return "Not found";
+
+    }
+
+    // Will be used later
+    public void addItem(String what, String where){
+        ItemsDB_map.put(what, where);
+    }
+
+    public void fillItemsDB(Context context, String filename) {
+        try {
+            BufferedReader reader = new BufferedReader( new InputStreamReader(context.getAssets().open(filename)) );
+            String line = reader.readLine();
+            while (line != null){
+                String[] what_where = line.split(", ");
+                String what = what_where[0];
+                String where = what_where[1];
+                ItemsDB_map.put(what, where);
+                line = reader.readLine();
+
+            }
+        } catch (IOException e) { }
+    }
+
+}
---
+++ GarbageV4-2025-Kotlin/app/src/main/java/dk/itu/GarbageV4/SortingActivity.java
@@ -0,0 +1,63 @@
+package dk.itu.GarbageV4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class SortingActivity extends AppCompatActivity {
+
+    //Shopping V1
+
+    // Model: Database of items
+    private static ItemsDB itemsDB;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.find_item);
+
+        ItemsDB.setContext(SortingActivity.this);
+
+        itemsDB= ItemsDB.get();
+
+        TextView items= findViewById(R.id.items);
+        EditText sortInput= findViewById(R.id.find_item_to_sort);
+        items.setText("Where to place item:");
+
+
+        Button listItems= findViewById(R.id.list_button);
+        listItems.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String itemWhat = sortInput.getText().toString();
+
+                String itemWhere = itemsDB.findCategory(itemWhat);
+
+                sortInput.setText(itemWhat + " should be placed in: " + itemWhere);
+
+            }
+        });
+        sortInput.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sortInput.setText("");
+            }
+        });
+
+        Button addItem= findViewById(R.id.add_new_item_button);
+        addItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SortingActivity.this, AddItemActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}

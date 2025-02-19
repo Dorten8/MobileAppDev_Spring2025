9452d28d5d4d2586dfebab1c1386fec9b196dc94
Dorten8 <jsej@itu.dk>
2025-02-19 12:36:02 +0100

    fixed the errors in AndroidManifest.xml

--- GarbageV3-2025/app/build.gradle.kts
+++ GarbageV3-2025/app/build.gradle.kts
@@ -32,6 +32,7 @@ android {
 }
 
 dependencies {
+    implementation ("com.google.android.material:material:1.9.0") // or latest version
 
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.8.0")
@@ -39,4 +40,5 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
 }
